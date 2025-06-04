570fc123171fa7fb606f57ff580b00b679a3f28e
Gabriel Marinho dos Santos <152182959+gMarinhum085@users.noreply.github.com>
2025-06-04 15:49:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ListaTarefas from "./components/ListaTarefas/index.jsx"
+import './main.css'
+export default function App(){
+  return(
+      <div className="body">
+        <ListaTarefas/>
+      </div>
+    )
+}
